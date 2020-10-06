aa1e0a8ffbd9f0e322a82c479393cec5a72f079e
Achiaga <alfonso.achiaga@gmail.com>
2020-10-06 16:53:25 +0200

    Updates

---
+++ precache-manifest.cb3907c352cd760a3e9d65a19dcb2b9d.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "78f45f14fe8293a80bffeb35a35d8651",
+    "url": "/star-wars-heroes/index.html"
+  },
+  {
+    "revision": "99de65c457dde236d9d5",
+    "url": "/star-wars-heroes/static/css/main.383b258e.chunk.css"
+  },
+  {
+    "revision": "6d2ca1b7b49dc6e89be9",
+    "url": "/star-wars-heroes/static/js/2.039320b7.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/star-wars-heroes/static/js/2.039320b7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "99de65c457dde236d9d5",
+    "url": "/star-wars-heroes/static/js/main.22a4011a.chunk.js"
+  },
+  {
+    "revision": "9e21613606c6f9bf8c1a",
+    "url": "/star-wars-heroes/static/js/runtime-main.19289104.js"
+  },
+  {
+    "revision": "557f1ddae5233609704ef3358b68c974",
+    "url": "/star-wars-heroes/static/media/light-big.557f1dda.jpg"
+  },
+  {
+    "revision": "247a81fb147a4d9ed4edbd4f9030c845",
+    "url": "/star-wars-heroes/static/media/light.247a81fb.png"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/star-wars-heroes/precache-manifest.cb3907c352cd760a3e9d65a19dcb2b9d.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/star-wars-heroes/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+});
